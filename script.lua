39126afceba5e2a18f42c1d0a7e098f166934846
RONIN Script <126873854+roninscript1122@users.noreply.github.com>
2023-10-12 17:00:33 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -320,11 +320,3 @@ addEventHandler("onClientResourceStart", resourceRoot,
 
 	end
 )
-
-
--- Sitemiz : https://sparrow-mta.blogspot.com/
--- Facebook : https://facebook.com/sparrowgta/
--- İnstagram : https://instagram.com/sparrowmta/
--- YouTube : https://youtube.com/c/SparroWMTA/
-
--- Discord : https://discord.gg/DzgEcvy
